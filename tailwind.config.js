8470938554779baa42f8719c2452d926596dde7b
Fernando Oliveira <112976898+FernandoFrali@users.noreply.github.com>
2023-04-16 03:07:03 -0300

    :lipstick: feat: styles configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,24 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'bg-dark': '#151d27',
+        'bg-light': '#f5f5f5',
+        'sp-60': '#FFFFFF',
+        'sp-30': '#E80C3E',
+        'sp-10': '#FF1100',
+        'sp-white2': '#F5F5F5',
+        'sp-gray': '#303030',
+        'sp-gray2': '#A5A4A4',
+      },
+      height: {
+        705: '705px',
+        575: '575px',
+      },
+      width: {
+        460: '460px',
+        386: '386px',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -16,4 +34,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
