094f8fec43c191d379910952c52cdbd8940b7a45
Fernando Oliveira <112976898+FernandoFrali@users.noreply.github.com>
2023-04-15 21:50:17 -0300

    :wrench: chore: configured jest and tailwindcss (+ added jest and RTL dependencie)

---
+++ jest.config.mjs
@@ -0,0 +1,16 @@
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: 'jest-environment-jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+}
+
+
+export default createJestConfig(config)
